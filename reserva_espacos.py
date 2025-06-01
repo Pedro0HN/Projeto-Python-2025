caff29146e69f579f7bb9ba92b473bcd1b143ae5
Pedro Neves <henriquepedro52919@gmail.com>
2025-06-01 13:16:33 -0300

    Add files via upload

---
+++ reserva_espacos.py
@@ -0,0 +1,32 @@
+with open("funcoes.py", "r", encoding="utf-8") as f:
+    codigo = f.read()
+exec(codigo)
+
+espacos_lista=[
+    ("Salão De Festas"),
+    ("Churrascaria"),
+    ("Salão Gourmet"),
+    ("Piscina")
+]
+reservas = []
+
+def menu():
+    while True:
+        print("\nMenu de Reservas")
+        print("1. Adicionar Reserva")
+        print("2. Listar Reservas")
+        print("3. Cancelar Reserva")
+        print("4. Sair")
+        opcao = input("Escolha uma opção: ")
+        if opcao == '1':
+            adicionar_reserva(reservas, espacos_lista)
+        elif opcao == '2':
+            listar_reservas(reservas)
+        elif opcao == '3':
+            cancelar_reserva(reservas, espacos_lista)
+        elif opcao == '4':
+            print("Saindo do sistema...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+menu()
